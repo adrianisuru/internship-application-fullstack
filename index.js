e703a77df24a56b6650e4496e915aeb21b5830e6
Adrian Herath <adrianisuru@gmail.com>
2020-04-19 22:54:09 -0500

    Distribute requests between variants

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const VARIANTS_URL = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+    return await fetch(VARIANTS_URL).then(response => { 
+        return response.json().then(obj => {
+            let variants = obj.variants
+            let idx = Math.floor(Math.random() * variants.length)
+            let variant = variants[idx]
+            return fetch(variant)
+        })
+    }).catch(e => {
+        console.log(e)
+    })
 }
